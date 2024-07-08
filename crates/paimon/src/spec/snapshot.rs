39c73827083c66db67e3a0ad7329fbcc50cb0155
QuakeWang <1677980708@qq.com>
2024-07-08 23:26:50 +0800

    update according the code review

--- crates/paimon/src/spec/snapshot.rs
+++ crates/paimon/src/spec/snapshot.rs
@@ -15,34 +15,14 @@
 // specific language governing permissions and limitations
 // under the License.
 
-/*!
- * Snapshots
- */
-
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use typed_builder::TypedBuilder;
-
-/// Type of changes in this snapshot.
-///
-/// Impl Reference: <https://github.com/apache/paimon/blob/db8bcd7fdd9c2705435d2ab1d2341c52d1f67ee5/paimon-core/src/main/java/org/apache/paimon/Snapshot.java#L506>.
-#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub enum CommitKind {
-    /// Change flushed from the mem table.
-    Append,
-    /// Changes by compacting existing data files.
-    Compact,
-    /// Changes that clear up the whole partition and then add new records.
-    Overwrite,
-    /// Collect statistics.
-    Analyze,
-}
 
 /// Snapshot for paimon.
 ///
 /// Impl Reference: <https://github.com/apache/paimon/blob/db8bcd7fdd9c2705435d2ab1d2341c52d1f67ee5/paimon-core/src/main/java/org/apache/paimon/Snapshot.java#L68>.
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize, TypedBuilder)]
+#[serde(rename_all = "camelCase")]
 pub struct Snapshot {
     /// version of snapshot
     version: i32,
@@ -54,22 +34,11 @@ pub struct Snapshot {
     delta_manifest_list: String,
     /// a manifest list recording all changelog produced in this snapshot
     #[builder(default = None)]
-    change_log_manifest_list: Option<String>,
+    changelog_manifest_list: Option<String>,
     /// a manifest recording all index files of this table
     #[builder(default = None)]
     index_manifest: Option<String>,
     commit_user: String,
-    /// Mainly for snapshot deduplication.
-    ///
-    /// If multiple snapshots have the same commitIdentifier, reading from any of these snapshots
-    /// must produce the same table.
-    ///
-    /// If snapshot A has a smaller commitIdentifier than snapshot B, then snapshot A must be
-    /// committed before snapshot B, and thus snapshot A must contain older records than snapshot B.
-    commit_identifier: i64,
-    commit_kind: CommitKind,
-    time_millis: i64,
-    log_offsets: HashMap<i32, i64>,
     /// record count of all changes occurred in this snapshot
     #[builder(default = None)]
     total_record_count: Option<i64>,
@@ -78,7 +47,7 @@ pub struct Snapshot {
     delta_record_count: Option<i64>,
     /// record count of all changelog produced in this snapshot
     #[builder(default = None)]
-    change_log_record_count: Option<i64>,
+    changelog_record_count: Option<i64>,
     /// watermark for input records
     #[builder(default = None)]
     watermark: Option<i64>,
@@ -114,33 +83,18 @@ impl Snapshot {
     }
 
     #[inline]
-    pub fn change_log_manifest_list(&self) -> Option<&String> {
-        self.change_log_manifest_list.as_ref()
+    pub fn changelog_manifest_list(&self) -> Option<&str> {
+        self.changelog_manifest_list.as_deref()
     }
 
     #[inline]
-    pub fn index_manifest(&self) -> Option<&String> {
-        self.index_manifest.as_ref()
+    pub fn index_manifest(&self) -> Option<&str> {
+        self.index_manifest.as_deref()
     }
 
     #[inline]
     pub fn commit_user(&self) -> &str {
         &self.commit_user
-    }
-
-    #[inline]
-    pub fn commit_identifier(&self) -> i64 {
-        self.commit_identifier
-    }
-
-    #[inline]
-    pub fn commit_kind(&self) -> &CommitKind {
-        &self.commit_kind
-    }
-
-    #[inline]
-    pub fn log_offsets(&self) -> &HashMap<i32, i64> {
-        &self.log_offsets
     }
 
     #[inline]
@@ -154,8 +108,8 @@ impl Snapshot {
     }
 
     #[inline]
-    pub fn change_log_record_count(&self) -> Option<i64> {
-        self.change_log_record_count
+    pub fn changelog_record_count(&self) -> Option<i64> {
+        self.changelog_record_count
     }
 
     #[inline]
@@ -164,7 +118,7 @@ impl Snapshot {
     }
 
     #[inline]
-    pub fn statistics(&self) -> Option<&String> {
-        self.statistics.as_ref()
+    pub fn statistics(&self) -> Option<&str> {
+        self.statistics.as_deref()
     }
 }
